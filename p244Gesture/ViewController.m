93eadeca925befbf0a138e7f211c2565744887a1
HanbinKim <hottuc@naver.com>
2014-01-07 17:40:24 +0900

    p244 Gesture 실습입니다.
    제스쳐를 이용해 이미지 확대 이동 축소 회전입니다.

--- p244Gesture/ViewController.m
+++ p244Gesture/ViewController.m
@@ -9,15 +9,46 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *appleImage;
 
 @end
 
 @implementation ViewController
 
+- (IBAction)handleRoate:(UIRotationGestureRecognizer *)gesture
+{
+    self.appleImage.transform = CGAffineTransformRotate(self.appleImage.transform, gesture.rotation);
+    [gesture setRotation:0.0];
+}
+
+- (void) handlePan : (UIPanGestureRecognizer *)gesture
+{
+    CGPoint point = [gesture locationInView:self.view];
+    self.appleImage.center = point;
+}
+
+- (void) handlePinch : (UIPinchGestureRecognizer *)gesture
+{
+    CGFloat scale = gesture.scale;
+    self.appleImage.transform = CGAffineTransformScale(self.appleImage.transform, scale, scale);
+    
+    [gesture setScale:1.0];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    // 코드로 사용할떄
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(handlePan:)];
+    [self.appleImage addGestureRecognizer:pan];
+    
+    UIPinchGestureRecognizer *pinch = [[UIPinchGestureRecognizer alloc]initWithTarget:self action:@selector(handlePinch:)];
+    [self.appleImage addGestureRecognizer:pinch];
+    
+    UIRotationGestureRecognizer *rotate = [[UIRotationGestureRecognizer alloc]initWithTarget:self action:@selector(handleRoate:)];
+    [self.appleImage addGestureRecognizer:rotate];
+    
 }
 
 - (void)didReceiveMemoryWarning
